f90b9dc7fe2218ef3ee7e99c638ee9c4b606712f
심도연 <shimdokite@gmail.com>
2023-10-14 00:46:18 +0900

    🎉 portfolio project start!

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'shimdokite&#39;s portfolio',
+	description: '프론트엔드 개발자 심도연의 포트폴리오',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
