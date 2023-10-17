623132b9e7de469ad11e977396546f0efdaf994a
심도연 <shimdokite@gmail.com>
2023-10-17 21:21:40 +0900

    🔧 Conf : 초기 전역 스타일 및 폰트 설정

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,22 +1,27 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Heebo } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const heebo = Heebo({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
-	title: 'shimdokite&#39;s portfolio',
-	description: '프론트엔드 개발자 심도연의 포트폴리오',
+  title: 'frontend developer Shimdoyeon',
+  description: '프론트엔드 개발자 심도연의 포트폴리오',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html>
+      <body className={heebo.className}>{children}</body>
+    </html>
+  );
 }
