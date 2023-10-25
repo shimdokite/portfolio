0e5dbf6b88968a17383a77a6ff635dd3741d2495
심도연 <shimdokite@gmail.com>
2023-10-26 01:06:40 +0900

    ✨ Feat : 전화번호 클립보드 복사 구현

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 
 import { CONTACT_ICONS, CONTACT_INFO } from '@/constants/contact';
@@ -8,28 +10,40 @@ interface ContactProps {
 
 export default function Contact({ type }: ContactProps) {
   const tooltip = type === 'phone' || type === 'mail';
+
+  const onClipBoard = (type: string) => {
+    const phoneNumber = CONTACT_INFO.phone;
+
+    if (type === 'phone') {
+      navigator.clipboard.writeText(phoneNumber as string);
+      alert('클립보드에 복사되었습니다 :)');
+    }
+  };
+
   return (
     <div className="relative group">
-      <div className="cursor-pointer w-10 h-10 border-blue-70 rounded-full bg-blue-70 flex justify-center items-center">
+      <button
+        type="button"
+        className="cursor-pointer w-10 h-10 border-blue-70 rounded-full bg-blue-70 flex justify-center items-center"
+        onClick={() => onClipBoard(type)}>
         <Image
           src={CONTACT_ICONS[type]}
           alt={`${type} icon`}
           width={type === 'tistory' ? 20 : 25}
           height={type === 'tistory' ? 20 : 25}
         />
-      </div>
+      </button>
 
       <div
         className={`${
           tooltip &&
-          'hidden rounded-md bg-black-10 group-hover:flex justify-center items-center z-[20] absolute top-[-100%] left-[50%] translate-x-[-50%]'
+          'hidden rounded-md bg-black-10 group-hover:flex justify-center items-center z-[20] absolute top-[-90%] left-[50%] translate-x-[-50%]'
         }`}>
-        <div
+        <p
           className="px-[7px] py-1 flex justify-center items-center w-full whitespace-nowrap text-sm text-center"
-          title="contact-tooltip"
           role="tooltip">
           {CONTACT_INFO[type]}
-        </div>
+        </p>
       </div>
     </div>
   );
