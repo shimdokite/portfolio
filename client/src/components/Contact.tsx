308e6d21154a4a469d9aa2ac7bcdf665f33dc517
심도연 <shimdokite@gmail.com>
2023-10-27 17:53:41 +0900

    ♻️ Refactor : Contact 컴포넌트 분리 및 조건부 스타일 추가

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -6,9 +6,10 @@ import { CONTACT_ICONS, CONTACT_INFO } from '@/constants/contact';
 
 interface ContactProps {
   type: 'phone' | 'github' | 'mail' | 'tistory';
+  component: 'intro' | 'footer';
 }
 
-export default function Contact({ type }: ContactProps) {
+export default function Contact({ type, component }: ContactProps) {
   const tooltip = type === 'phone' || type === 'mail';
 
   const onClipBoard = (type: string) => {
@@ -24,7 +25,11 @@ export default function Contact({ type }: ContactProps) {
     <div className="relative group">
       <button
         type="button"
-        className="cursor-pointer w-10 h-10 border-blue-70 rounded-full bg-blue-70 flex justify-center items-center"
+        className={`${
+          component === 'intro'
+            ? 'border-pink-70 bg-pink-70'
+            : 'border-blue-70 bg-blue-70'
+        } cursor-pointer w-10 h-10 rounded-full flex justify-center items-center`}
         onClick={() => onClipBoard(type)}>
         <Image
           src={CONTACT_ICONS[type]}
@@ -40,7 +45,7 @@ export default function Contact({ type }: ContactProps) {
           'hidden rounded-md bg-black-10 group-hover:flex justify-center items-center z-[20] absolute top-[-90%] left-[50%] translate-x-[-50%]'
         }`}>
         <p
-          className="px-[7px] py-1 flex justify-center items-center w-full whitespace-nowrap text-sm text-center"
+          className="px-[7px] py-1 flex justify-center items-center w-full whitespace-nowrap text-sm text-center font-normal"
           role="tooltip">
           {CONTACT_INFO[type]}
         </p>
