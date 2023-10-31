a8164489a7f55a6a289d625fdbb59eec331d4242
심도연 <shimdokite@gmail.com>
2023-10-31 23:09:03 +0900

    💄 Style : Button 컴포넌트, black 10 색상 변경

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 export default function Button({ children }: ButtonProps) {
   return (
     <button className="rounded-2xl bg-pink-70" type="button" role="button">
-      <p className="py-1 px-[10px] text-white-10 text-[14px] text-center">
+      <p className="py-1 px-[10px] text-white-10 text-[14px] text-center font-bold">
         {children}
       </p>
     </button>
