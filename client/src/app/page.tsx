b77581801381f34af86aff1360fd14c484a5a218
심도연 <shimdokite@gmail.com>
2023-11-18 15:39:20 +0900

    ✨ Project 컴포넌트 구현

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Intro, Stack, Experience } from '../components';
+import { Intro, Stack, Project, Experience } from '../components';
 
 export default function Home() {
   return (
     <div className="font-bold text-blue-90">
       <Intro />
       <Stack />
+      <Project />
       <Experience />
     </div>
   );
---
+++ client/src/components/Project.tsx
@@ -0,0 +1,9 @@
+import { ProjectTemplate } from '.';
+
+export default function Project() {
+  return (
+    <div className="h-[582px] flex justify-center items-center">
+      <ProjectTemplate />
+    </div>
+  );
+}
