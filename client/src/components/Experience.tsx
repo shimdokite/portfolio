c7dac2009849edf9279d827ac9b51abb7c4c8eb5
심도연 <shimdokite@gmail.com>
2023-12-01 02:41:08 +0900

    💄 모바일 반응형 구현

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -2,31 +2,43 @@ import { EXPERIENCES } from '@/constants/experience';
 
 export default function Experience() {
   return (
-    <div className="h-[582px] bg-pink-10">
-      <h1 className="relative top-10 left-36 text-2xl">Experience</h1>
+    <div className="h-[615px] bg-pink-10 max-[810px]:h-[650px]">
+      <h1 className="relative top-10 left-36 text-2xl max-[410px]:left-[115px]">
+        Experience
+      </h1>
 
-      <div className="mt-14 flex flex-col justify-center items-center">
+      <div className="w-full h-[85%] flex flex-col justify-center items-center mx-2 max-[810px]:mt-0">
         {EXPERIENCES.map((experience) => (
-          <div className="mt-20 w-[600px] flex gap-16" key={experience.title}>
-            <div className="w-[200px]">
-              <section className="flex flex-col items-center">
+          <div
+            className="w-[500px] mt-14 flex justify-center items-center gap-14 max-[810px]:gap-9 max-[810px]:flex-col max-[810px]:mt-16"
+            key={experience.title}>
+            <section className="flex flex-col justify-center items-center mr-7">
+              <div className="flex flex-col justify-center items-center">
                 <h2>{experience.title}</h2>
 
                 <div className="font-thin text-sm">
                   <p>{experience.start} -</p>
                   <p>{experience.end}</p>
                 </div>
-              </section>
-            </div>
+              </div>
+            </section>
 
-            <section>
-              <h2>{experience.subTitle}</h2>
+            <section className="flex flex-col justify-center items-center">
+              <div className="max-[810px]:flex flex-col">
+                <div className="flex justify-start">
+                  <h2 className="max-[810px]:flex justify-center items-center">
+                    {experience.subTitle}
+                  </h2>
+                </div>
 
-              <ul className="w-fit flex flex-col justify-start font-normal text-sm list-disc">
-                {experience.features.map((feature) => (
-                  <li key={feature}>{feature}</li>
-                ))}
-              </ul>
+                <ul className="flex flex-col justify-start font-normal text-sm list-disc">
+                  {experience.features.map((feature) => (
+                    <li className="w-full" key={feature}>
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </section>
           </div>
         ))}
--- client/src/components/Intro.tsx
+++ client/src/components/Intro.tsx
@@ -5,25 +5,35 @@ import { INTRO_TEXT } from '@/constants/intro';
 
 export default function Intro() {
   return (
-    <div className="h-[658px] flex justify-center items-center">
-      <div className="mt-14 w-[48%] flex flex-col items-start gap-4">
-        <h1 className="text-[44px] leading-tight font-bold whitespace-pre-line">
+    <div className="w-full h-[690px] flex justify-center items-center max-[450px]:flex  max-[450px]:flex-col max-[450px]:h-[812px]">
+      <Image
+        src="/assets/image/profile.jpg"
+        alt="profile image"
+        width={300}
+        height={300}
+        className="rounded-full max-w-[250px] max-h-[250px] min-w-[150px] min-h-[150px] drop-shadow-imageShadow mx-2 min-[450px]:hidden"
+      />
+
+      <section className="mt-14 w-[45%] flex flex-col items-start gap-4 mx-2 max-[450px]:w-[90%]">
+        <h1 className="text-[44px] max-[700px]:text-[34px] max-[550px]:text-[24px] max-[450px]:text-[22px] leading-tight font-bold whitespace-pre-line">
           {INTRO_TEXT.title}
         </h1>
 
-        <h2 className="font-normal whitespace-pre-line mb-5">
-          {INTRO_TEXT.description}
+        <h2 className="max-[660px]:text-[13px] max-[550px]:text-[12px] font-normal whitespace-pre-line mb-3 mr-[10px]">
+          &nbsp;{INTRO_TEXT.description}
         </h2>
 
-        <Contacts component="intro" />
-      </div>
+        <div className="w-full flex justify-between items-center">
+          <Contacts component="intro" />
+        </div>
+      </section>
 
       <Image
-        src="/assets/image/profile.png"
+        src="/assets/image/profile.jpg"
         alt="profile image"
-        width={243}
-        height={243}
-        className="drop-shadow-imageShadow"
+        width={300}
+        height={300}
+        className="rounded-full max-w-[250px] max-h-[250px] min-w-[150px] min-h-[150px] drop-shadow-imageShadow mx-2 max-[450px]:hidden"
       />
     </div>
   );
