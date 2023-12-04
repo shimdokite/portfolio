9bf26ae399a6821196dff03c5717827a5617d9e3
심도연 <shimdokite@gmail.com>
2023-12-05 01:19:32 +0900

    🐛 모바일 contact 잘림 이슈 해결 및 스크롤 위치 수정

--- client/utils/getScrollForwardMobile.ts
+++ client/utils/getScrollForwardMobile.ts
@@ -1,8 +1,8 @@
 const getScrollForwardMobile = (type: string) => {
   if (type === 'contact') return 0;
   if (type === 'stack') return 750;
-  if (type === 'project') return 1550;
-  if (type === 'experience') return 2360;
+  if (type === 'project') return 1560;
+  if (type === 'experience') return 2370;
 };
 
 export default getScrollForwardMobile;
