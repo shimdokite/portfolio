d3c02b330221f7cf25049812cabfb7b6835f9798
심도연 <shimdokite@gmail.com>
2023-12-19 22:36:03 +0900

    ♻️ 프로필 이미지 변경

--- client/src/components/Intro.tsx
+++ client/src/components/Intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
   return (
     <div className="w-full h-[690px] flex justify-center items-center max-[450px]:flex  max-[450px]:flex-col max-[450px]:h-[812px]">
       <Image
-        src="/assets/image/profile.jpg"
+        src="/assets/image/profile.png"
         alt="profile image"
         width={300}
         height={300}
@@ -29,7 +29,7 @@ export default function Intro() {
       </section>
 
       <Image
-        src="/assets/image/profile.jpg"
+        src="/assets/image/profile.png"
         alt="profile image"
         width={300}
         height={300}
